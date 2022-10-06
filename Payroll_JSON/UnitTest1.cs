6588bffd5de92058f74087d27aa37c02d5c6cb0f
Sandeep singh <sandy180388@gmail.com>
2022-10-06 08:53:05 +0530

    [sandeep] added udate file in UC_4

--- Payroll_JSON/UnitTest1.cs
+++ Payroll_JSON/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Newtonsoft.Json;
 using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Net;
 
@@ -33,5 +34,69 @@ namespace Payroll_JSON
             foreach (var mem in list)
                 System.Console.WriteLine(mem.id + " " + mem.first_name + " " + mem.last_name + " " + mem.email);
         }
+        //test the insertion
+        [Test]
+        public void TestMethod_ToTest_InsertData_JSONServer()
+        {
+            RestRequest request = new RestRequest("/employees", Method.Post);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.Add("first_name", "Ram");
+            jsonObject.Add("last_name", "Kumar");
+            jsonObject.Add("salary", 25000);
+            request.AddParameter("application/json", jsonObject, ParameterType.RequestBody);
+            IRestResponse response = restClient.Execute(request);
+            var result = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Ram", result.first_name);
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+        }
+        //add data to json server
+        public void add(JsonObject jsonObject)
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            request.AddParameter("application/json", jsonObject, ParameterType.RequestBody);
+            IRestResponse response = restClient.Execute(request);
+
+        }
+        [Test]
+        public void TestMethod_ToTest_MultipleInsertion()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            List<JsonObject> list = new List<JsonObject>();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.Add("first_name", "Priya");
+            jsonObject.Add("last_name", "Devi");
+            jsonObject.Add("salary", 23000);
+            list.Add(jsonObject);
+            JsonObject jsonObject1 = new JsonObject();
+            jsonObject1.Add("first_name", "Dhanush");
+            jsonObject1.Add("last_name", "Raj");
+            jsonObject1.Add("salary", 32000);
+            list.Add(jsonObject1);
+            foreach (var mem in list)
+                add(mem);
+            //retrieve to check the count after insertion
+            IRestResponse response = GetAllEmployee();
+            List<Employee> result = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(9, result.Count);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+        //method to update values for given id
+        [Test]
+        public void Test_Method_To_Update()
+        {
+            Employee employee = new Employee();
+            RestRequest request = new RestRequest("/employees/9", Method.PUT);
+            JsonObject json = new JsonObject();
+            json.Add("first_name", "Danny");
+            json.Add("last_name", "Raj");
+            json.Add("salary", 30000);
+            request.AddJsonBody(json);
+            IRestResponse response = restClient.Execute(request);
+            var result = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual(result.first_name, "Dhanush");
+        }
+
+
     }
 }
